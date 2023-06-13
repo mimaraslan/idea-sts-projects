480ea2841e24e448041d1b698b4b8f4989633feb
Mimar Aslan <mimaraslan@gmail.com>
2023-06-14 00:26:10 +0300

    Serialization Deserialization

--- Demo/src/main/java/com/mimaraslan/AppMain.java
+++ Demo/src/main/java/com/mimaraslan/AppMain.java
@@ -1,5 +1,6 @@
 package com.mimaraslan;
 
+
 import java.io.*;
 
 // Serialization Deserialization
--- _053_SerializationDeserialization/src/main/java/com/mimaraslan/Personel.java
+++
@@ -1,8 +0,0 @@
-package com.mimaraslan;
-
-import java.io.Serializable;
-
-public class Personel implements Serializable {
-
-    String adi;
-}
