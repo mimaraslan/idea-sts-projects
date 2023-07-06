4bbdfdebf4e115187074290b3e3d85927d7f71eb
Mimar Aslan <mimaraslan@gmail.com>
2023-07-06 19:24:09 +0300

    SQL - Structured Query Language

--- _058_SQL_StructuredQueryLanguage/src/main/resources/Day_01_/2023_07_04_sorgular_myCompanySql.sql
+++ _058_SQL_StructuredQueryLanguage/src/main/resources/Day_01_/2023_07_04_sorgular_myCompanySql.sql
@@ -1,5 +1,7 @@
 CREATE SCHEMA `companydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
+
 -- =======================
+
 CREATE TABLE `companydb`.`customers` (
   `customer_id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(50) NOT NULL,
@@ -8,8 +10,13 @@ CREATE TABLE `companydb`.`customers` (
   `country` VARCHAR(45) NULL,
   PRIMARY KEY (`customer_id`))
 ENGINE = InnoDB;
+
 -- =======================
+
+/*
 INSERT INTO `companydb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Burak', 'Delice', '55555', 'TR');
 INSERT INTO `companydb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Mehmet', 'Caner', '66666', 'UK');
+*/
 -- =======================
+
 SELECT * FROM companydb.customers;
