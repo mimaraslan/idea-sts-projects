3bc443fefb70d6fd7c48bf9bdc24779a4cfc8a8f
Mimar Aslan <mimaraslan@gmail.com>
2023-08-02 14:55:31 +0300

    Spring Boot XML Config
    Spring Boot XML Config

--- _077_SpringBoot_Xml_Config/src/main/java/com/mimaraslan/Application.java
+++ _077_SpringBoot_Xml_Config/src/main/java/com/mimaraslan/Application.java
@@ -5,16 +5,18 @@ import com.mimaraslan.service.MessageProcessorImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({"classpath*:applicationContext.xml"})
 public class Application {
 
     public static void main(String[] args) {
 
-
      //  SpringApplication.run(Application.class, args);
 
         ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+
         MessageProcessor messageProcessor =  applicationContext.getBean(MessageProcessor.class);
         messageProcessor.processingMsg("Selamlar nasılsın?");
 
