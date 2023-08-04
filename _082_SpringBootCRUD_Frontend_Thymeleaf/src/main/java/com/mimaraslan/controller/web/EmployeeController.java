442bdd05393f68c45e78e634ef2969295cd01fa0
Mimar Aslan <mimaraslan@gmail.com>
2023-08-04 17:12:33 +0300

    Spring Boot CRUD
    Spring Boot CRUD

--- _082_SpringBootCRUD_Frontend_Thymeleaf/src/main/java/com/mimaraslan/controller/web/EmployeeController.java
+++ _082_SpringBootCRUD_Frontend_Thymeleaf/src/main/java/com/mimaraslan/controller/web/EmployeeController.java
@@ -1,11 +1,15 @@
 package com.mimaraslan.controller.web;
 
 
+import com.mimaraslan.model_entity.Employee;
 import com.mimaraslan.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller   // Web sayfaları ile Java konuşacak.
 @RequestMapping("/")
@@ -17,14 +21,23 @@ public class EmployeeController {
 
     //   http://localhost:8080/list
     @RequestMapping({"/","/list"}) //@GetMapping
-    public String getAllEmployees(){
+    public String getAllEmployees(Model model){
+
+        List<Employee> listEmployees = employeeService.getAllEmployees();
+
+        model.addAttribute("listEmployees", listEmployees);
+
+       /* for ( Employee employee: listEmployees ) {
+            System.out.println(employee);
+        }*/
+
         return "list-employees";
     }
 
 
     //   http://localhost:8080/add
     @RequestMapping("/add") //@GetMapping
-    public String getAddEmployee(){
+    public String getAddEmployee(Model model){
         return "add-employees";
     }
 
--- _082_SpringBootCRUD_Frontend_Thymeleaf/src/main/java/com/mimaraslan/service/EmployeeService.java
+++ _082_SpringBootCRUD_Frontend_Thymeleaf/src/main/java/com/mimaraslan/service/EmployeeService.java
@@ -1,13 +1,33 @@
 package com.mimaraslan.service;
 
 
+import com.mimaraslan.model_entity.Employee;
 import com.mimaraslan.repository_dao.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EmployeeService {
 
     @Autowired
     EmployeeRepository employeeRepository;
+
+
+    public List<Employee> getAllEmployees() {
+/*
+        List<Employee> listEmployees = employeeRepository.findAll();
+
+        if(listEmployees.size()>0)
+            return listEmployees;
+        else
+            return new ArrayList<Employee>();
+        */
+
+        return employeeRepository.findAll();
+
+
+    }
 }
