7007f1a231a24be0f628edad31c91ad9b3a544f4
Mimar Aslan <mimaraslan@gmail.com>
2023-08-09 14:19:25 +0300

    Spring Boot CRUD - MVC - Thymeleaf - Bootstrap
    Spring Boot CRUD - MVC - Thymeleaf - Bootstrap

--- _082_SpringBootCRUD_Frontend_Thymeleaf/src/main/java/com/mimaraslan/controller/web/EmployeeController.java
+++ _082_SpringBootCRUD_Frontend_Thymeleaf/src/main/java/com/mimaraslan/controller/web/EmployeeController.java
@@ -1,16 +1,19 @@
 package com.mimaraslan.controller.web;
 
 
+import com.mimaraslan.exception.RecordNotFoundException;
 import com.mimaraslan.model_entity.Employee;
 import com.mimaraslan.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller   // Web sayfaları ile Java konuşacak.
 @RequestMapping("/")
@@ -26,36 +29,42 @@ public class EmployeeController {
     public String getAllEmployees(Model model){
 
         List<Employee> listEmployees = employeeService.getAllEmployees();
-
         model.addAttribute("listEmployees", listEmployees);
 
        /* for ( Employee employee: listEmployees ) {
             System.out.println(employee);
         }*/
-
         return "list-employees";
     }
 
 
-
-
-    //   http://localhost:8080/add
-    @RequestMapping({"/edit", "/edit/{id}"}) //@GetMapping
-    public String getEditEmployee(Model model){
-        return "add-edit-employees";
+    // http://localhost:8080/add
+    // http://localhost:8080/edit/1
+    @RequestMapping(path = {"/edit", "/edit/{id}"})
+    public String editEmployeeById(Model model, @PathVariable("id") Optional<Long> id) throws  RecordNotFoundException {
+        if (id.isPresent()) {
+            Employee entity = employeeService.getEmployeeById(id.get());
+            model.addAttribute("employee", entity);
+        } else {
+            model.addAttribute("employee", new Employee());
+        }
+        return "add-edit-employee";
     }
 
-
     //   http://localhost:8080/createEmployee
-    //@PostMapping(value = "/createEmployee")
-    @RequestMapping(value = "/createEmployee", method = RequestMethod.POST)
-    public String createOrUpdateEmployee(Employee employee){
-
+    @RequestMapping(path = "/createEmployee", method = RequestMethod.POST)
+    //  @PostMapping("/createEmployee")
+    public String createOrUpdateEmployee(Employee employee) {
         employeeService.createOrUpdateEmployee(employee);
-
         return "redirect:/";
     }
 
+
+    @RequestMapping(path = "/delete/{id}")
+    public String deleteEmployeeById(Model model, @PathVariable("id") Long id)  throws  RecordNotFoundException {
+        employeeService.deleteEmployeeById(id);
+        return "redirect:/";
+    }
 
 
 }
--- _082_SpringBootCRUD_Frontend_Thymeleaf/src/main/java/com/mimaraslan/service/EmployeeService.java
+++ _082_SpringBootCRUD_Frontend_Thymeleaf/src/main/java/com/mimaraslan/service/EmployeeService.java
@@ -1,13 +1,16 @@
 package com.mimaraslan.service;
 
 
+import com.mimaraslan.exception.RecordNotFoundException;
 import com.mimaraslan.model_entity.Employee;
 import com.mimaraslan.repository_dao.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -15,21 +18,38 @@ public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
 
-
     public List<Employee> getAllEmployees() {
-/*
+
         List<Employee> listEmployees = employeeRepository.findAll();
 
         if(listEmployees.size()>0)
             return listEmployees;
         else
             return new ArrayList<Employee>();
-        */
 
-        return employeeRepository.findAll();
-
-
+       // return employeeRepository.findAll();
     }
+
+
+    public Employee getEmployeeById(Long id) throws RecordNotFoundException {
+
+        Optional<Employee> employee = employeeRepository.findById(id);
+
+        if (employee.isPresent()) {
+            return employee.get();
+        } else {
+            throw new RecordNotFoundException("No employee record exist for given ID : " + id);
+            //   throw new ResourceNotFoundException("No employee record exist for given ID : " + id);
+        }
+
+     /*
+        Optional<Employee> employee = Optional.ofNullable(employeeRepository.findById(id)
+                .orElseThrow(   () -> new ResourceNotFoundException("Employee not found ID : " + id)    ));
+        return employee.get();
+      */
+    }
+
+
 
     public Employee createOrUpdateEmployee(Employee employee) {
 
@@ -39,13 +59,30 @@ public class EmployeeService {
             return employee;
         }
         else {  // EDIT - UPDATE
+            Optional<Employee> employeeInfo = employeeRepository.findById(employee.getId());
 
-
-
-
-            return  employee;
+            if (employeeInfo.isPresent()) {
+                Employee newEntity = employeeInfo.get();
+                newEntity.setEmail(employee.getEmail());
+                newEntity.setFirstName(employee.getFirstName());
+                newEntity.setLastName(employee.getLastName());
+                newEntity = employeeRepository.save(newEntity);
+                return newEntity;
+            } else {
+                employee = employeeRepository.save(employee);
+                return employee;
+            }
         }
-
-
     }
+
+    public void deleteEmployeeById(Long id) throws  RecordNotFoundException {
+        Optional<Employee> employee = employeeRepository.findById(id);
+
+        if (employee.isPresent()) {
+            employeeRepository.deleteById(id);
+        } else {
+            throw new RecordNotFoundException("No employee record exist for given id");
+        }
+    }
+
 }
