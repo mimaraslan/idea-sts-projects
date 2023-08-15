bcdedd0cc1e33a38bf5e8f787ece6facfb16893e
Mimar Aslan <mimaraslan@gmail.com>
2023-08-15 17:33:38 +0300

    Docker Tag versions
    Docker Tag versions

---
+++ _084_Docker_Versions/src/main/java/com/mimaraslan/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.mimaraslan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello docker";
+    }
+
+    @GetMapping("/metod2")
+    public String metod2() {
+        return "Metod 2";
+    }
+    @GetMapping("/metod3")
+    public String metod3() {
+        return "Metod 3";
+    }
+}
