e89e80213a260a7d44ecb56c424a6fa6e03ae180
Mimar Aslan <mimaraslan@gmail.com>
2023-08-15 17:20:58 +0300

    Hello Docker
    Hello Docker

--- _083_Docker001-hello/src/main/java/com/mimaraslan/HelloController.java
+++ _083_Docker001-hello/src/main/java/com/mimaraslan/HelloController.java
@@ -6,13 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 // http://localhost:8080/api/v1
+// http://localhost:8080/surum1
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping({"/api/v1", "/surum1"})
 public class HelloController {
 
     //  http://localhost:8080/api/v1/hello
+    //  http://localhost:8080/surum1/hello
     @GetMapping("/hello")
     public String hello() {
         return "Hello Docker";
     }
+
+
 }
