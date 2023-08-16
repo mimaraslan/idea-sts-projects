01ff90fb5ad061802522a832f41c687117165006
Mimar Aslan <mimaraslan@gmail.com>
2023-08-16 19:40:33 +0300

    Java Microservices Demo
    Java Microservices Demo

--- _086_JavaMicroservicesDemo/AuthService/src/main/java/com/mimaraslan/AuthService.java
+++ _086_JavaMicroservicesDemo/AuthService/src/main/java/com/mimaraslan/AuthService.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package com.mimaraslan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class AuthService {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(AuthService.class);
     }
-
 }
