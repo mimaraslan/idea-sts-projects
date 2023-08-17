cd58205ed26ffed994a10a9886182aa5b3eeb860
Mimar Aslan <mimaraslan@gmail.com>
2023-08-17 16:53:08 +0300

    Java Microservices Demo
    Java Microservices Demo

--- _086_JavaMicroservicesDemo/AuthService/src/main/java/com/mimaraslan/service/AuthService.java
+++ _086_JavaMicroservicesDemo/AuthService/src/main/java/com/mimaraslan/service/AuthService.java
@@ -4,13 +4,13 @@ package com.mimaraslan.service;
 import com.mimaraslan.repository.IAuthRepository;
 import com.mimaraslan.repository.entity.Auth;
 import com.mimaraslan.utility.ServiceManager;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService extends ServiceManager <Auth, Long> {
 
     private final IAuthRepository repository;
+
     public AuthService(IAuthRepository repository) {
         super(repository);
         this.repository = repository;
