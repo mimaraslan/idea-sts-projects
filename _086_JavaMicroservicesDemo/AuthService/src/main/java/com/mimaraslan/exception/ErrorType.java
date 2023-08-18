6eeae74c2ae023cbbeea2c46796e25534d10cada
Mimar Aslan <mimaraslan@gmail.com>
2023-08-18 14:45:04 +0300

    Java Microservices Demo
    Java Microservices Demo

--- _086_JavaMicroservicesDemo/AuthService/src/main/java/com/mimaraslan/exception/ErrorType.java
+++ _086_JavaMicroservicesDemo/AuthService/src/main/java/com/mimaraslan/exception/ErrorType.java
@@ -14,7 +14,12 @@ public enum ErrorType {
 
     MUSTERI_BULUNAMADI(1003,"Aradığınız müşteri sistemde kayıtlı değil", NOT_FOUND),
     URUN_EKLEME_HATASI(2001,"Ürün ekleme başarısız oldu", INTERNAL_SERVER_ERROR),
-    INVALID_PARAMETER(3001,"Geçersiz parametre girişi yaptınız", BAD_REQUEST);
+    INVALID_PARAMETER(3001,"Geçersiz parametre girişi yaptınız", HttpStatus.BAD_REQUEST),
+
+    REGISTER_PASSWORD_MISMACTH(4001,"Girilen parolalar eşleşmedi.", HttpStatus.BAD_REQUEST),
+    REGISTER_USERNAME_EXISTS(4002,"Geçersiz parametre girişi yaptınız", HttpStatus.BAD_REQUEST),
+
+    LOGIN_USERNAME_OR_PASSWORD_NOT_EXISTS(5001,"Girilen kullanıcı adı veya parola hatalı.", HttpStatus.BAD_REQUEST);
 
     private int code;
     private String message;
