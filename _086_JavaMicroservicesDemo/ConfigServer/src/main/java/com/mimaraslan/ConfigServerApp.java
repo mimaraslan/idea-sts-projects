b9365afbb03f291acb47f6067a878a5bf320c72b
Mimar Aslan <mimaraslan@gmail.com>
2023-08-23 15:25:59 +0300

    Java Microservices Demo
    Java Microservices Demo

--- _086_JavaMicroservicesDemo/ConfigServer/src/main/java/com/mimaraslan/ConfigServerApp.java
+++ _086_JavaMicroservicesDemo/ConfigServer/src/main/java/com/mimaraslan/ConfigServerApp.java
@@ -1,7 +1,13 @@
 package com.mimaraslan;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ConfigServerApp.class);
     }
 }
