21398de3ea8402f8f36aad9eac0cb5b3b3e56fb9
Mimar Aslan <mimaraslan@gmail.com>
2023-08-24 15:59:49 +0300

    Circuit Breaker
    Circuit Breaker

--- _086_JavaMicroservicesDemo/ApiGatewayService/src/main/java/com/mimaraslan/controller/FallbackController.java
+++ _086_JavaMicroservicesDemo/ApiGatewayService/src/main/java/com/mimaraslan/controller/FallbackController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//   http://localhost:9090/fallback
+
 @RestController
 @RequestMapping("/fallback")
 public class FallbackController {
@@ -16,11 +16,22 @@ public class FallbackController {
         return ResponseEntity.ok("Auth Service: Şu anda geçici olarak hizmet verememekteyiz.");
     }
 
-    // http://localhost:9090/fallback/sale
+    // http://localhost:9091/fallback/product
+    @GetMapping("/product")
+    public  ResponseEntity<String>  fallbackProduct() {
+        return ResponseEntity.ok("Product Service: Şu anda geçici olarak hizmet verememekteyiz.");
+    }
+
+    // http://localhost:9092/fallback/sale
     @GetMapping("/sale")
     public  ResponseEntity<String>  fallbackSale() {
         return ResponseEntity.ok("Sale Service: Şu anda geçici olarak hizmet verememekteyiz.");
     }
 
 
+    // http://localhost:9093/fallback/user
+    @GetMapping("/user")
+    public  ResponseEntity<String>  fallbackUser() {
+        return ResponseEntity.ok("User Service: Şu anda geçici olarak hizmet verememekteyiz.");
+    }
 }
